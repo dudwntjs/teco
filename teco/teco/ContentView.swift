dccfec5666d7e9d2b35b43a827bbc8a645fddb26
sun <sts07190@naver.com>
2026-04-19 22:51:20 +0900

    [setting] 프로젝트 초기 세팅

---
+++ teco/teco/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  teco
+//
+//  Created by sun on 4/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
